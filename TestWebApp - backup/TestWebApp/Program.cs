cb63696e2b263995af8d1e2f0302a8000128974b
VladimirRepp <57412538+VladimirRepp@users.noreply.github.com>
2025-05-21 16:02:48 +0700

    Update Program.cs

--- TestWebApp - backup/TestWebApp/Program.cs
+++ TestWebApp - backup/TestWebApp/Program.cs
@@ -27,7 +27,7 @@ namespace TestWebApp
             app.MapStaticAssets();
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}")
+                pattern: "{controller=Home}/{action=Index}/{id?}") // set startup controller here
                 .WithStaticAssets();
 
             app.Run();
